d9efd86cbb66f4fb16bcfc323678a40cadb24b4b
Techcable <Techcable@techcable.net>
2017-12-15 14:02:14 -0700

    Fix serialization code

--- src/serialize.rs
+++ src/serialize.rs
@@ -3,19 +3,21 @@ use ::{AutoError};
 
 /// Serialize the specified value into lz4 compressed bincode
 #[inline]
-#[cfg(all(feature="bincode", feature="lz4)"))]
-pub fn serialize_compressed_bincode<T: Serialize, W: Write>(value: &T, write: W) -> Result<(), SerializationError> {
-    use std::io::{BufReader, BufWriter};
-    let mut encoder = BufWriter::new(::lz4::encoder::EncoderBuilder::new().build(write)?);
+#[cfg(all(feature="bincode", feature="lz4"))]
+pub fn serialize_compressed_bincode<T, W>(value: &T, write: W) -> Result<(), SerializationError>
+    where T: ::serde::Serialize, W: ::std::io::Write {
+    let mut encoder = ::std::io::BufWriter::new(
+        ::lz4::EncoderBuilder::new().build(write)?);
     ::bincode::serialize_into(&mut encoder, value, ::bincode::Infinite)?;
     Ok(())
 }
 /// Deserialize the specified value from lz4 compresed bincode
 #[inline]
-#[cfg(all(feature="bincode", feature="lz4)"))]
-pub fn deserialize_compressed_bincode<T: DeserializeOwned, R: Read>(source: R) -> Result<T, SerializationError> {
-    use std::io::{BufReader, BufWriter};
-    let mut decoder = BufReader::new(::lz4::decoder::Decoder::new(source)?);
+#[cfg(all(feature="bincode", feature="lz4"))]
+pub fn deserialize_compressed_bincode<T, R>(source: R) -> Result<T, SerializationError>
+    where T: ::serde::de::DeserializeOwned, R: ::std::io::Read {
+    let mut decoder = ::std::io::BufReader::new(
+        ::lz4::Decoder::new(source)?);
     Ok(::bincode::deserialize_from(&mut decoder, ::bincode::Infinite)?)
 }
 
